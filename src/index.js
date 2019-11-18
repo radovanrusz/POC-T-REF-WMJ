7b76acd719018df21d7673eaf7d6f91365a61e03
Jan Nemec <jan_nemec@cz.ibm.com>
2019-11-18 19:57:35 +0100

    Kafka addition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const port = process.env.PORT
+require('./kafka')
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
---
+++ src/kafka.js
@@ -0,0 +1,40 @@
+const kafkaHost = process.env.KAFKA_HOST
+const kafkaTopic = process.env.KAFKA_TOPIC
+const kafka = require('kafka-node')
+const JournalRec = require('./models/journalrec')
+
+try {
+    console.log('Kafka Consumer is booting up')
+    const client = new kafka.KafkaClient(kafkaHost)
+    const topics = [
+        {
+            topic: kafkaTopic, 
+            partition: 0
+        }
+    ]
+    const options = {
+        autoCommit: true, 
+        fetchMaxWaitMs: 1000, 
+        fetchMaxBytes: 1024 * 1024, 
+        encoding: 'utf8', 
+        fromOffset: false
+    }
+    
+    const consumer = new kafka.Consumer(client, topics, options)
+
+    consumer.on('message', async (message) => {    
+        const journalrec = new JournalRec(JSON.parse(message.value))
+        try {
+            await journalrec.save()
+            console.log('Journal record saved successfully')
+        }catch(e) {
+            console.log(e)
+        }
+    })
+
+    consumer.on('error', (err) => {
+        console.log('error', err)
+    })
+}catch(e) {
+    console.log(e)
+}
