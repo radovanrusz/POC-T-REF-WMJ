af32dcaf8503a9adab48242fc6b44aa7341a6562
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2019-11-20 12:56:38 +0100

    Update kafka.js

--- src/kafka.js
+++ src/kafka.js
@@ -4,7 +4,7 @@ const kafka = require('kafka-node')
 const JournalRec = require('./models/journalrec')
 
 try {
-    console.log('Kafka Consumer is booting up')
+    console.log('Kafka Consumer is booting up ... (kafkaHost:'+kafkaHost + ' kafkaTopic:' + kafkaTopic + ')')
     const client = new kafka.KafkaClient(kafkaHost)
     const topics = [
         {
