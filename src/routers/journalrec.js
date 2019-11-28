0fd2c8dd2db41ae8ab28ab49655e6c50861661cd
Juraj Piovar <jpiovar@sk.ibm.com>
2019-11-28 13:27:04 +0100

    jp asyncForEach initialJournal

--- src/routers/journalrec.js
+++ src/routers/journalrec.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const JournalRec = require('../models/journalrec')
-const router = new express.Router()
+const express = require('express');
+const JournalRec = require('../models/journalrec');
+const router = new express.Router();
+const asyncForEach = require ('../helpers/asyncForEach.helper');
 
 router.post('/journal', async (req, res) => {
     const journalrec = new JournalRec(req.body)
@@ -14,19 +15,28 @@ router.post('/journal', async (req, res) => {
 
 router.get('/journal', async (req, res) => {
     try {
-        const journal = await JournalRec.find({})
-        res.send(journal)
+        const journal = await JournalRec.find({});
+        res.json(journal);
     } catch(e) {
-        res.status(500).send()
+        res.status(500).json({ error: e });
     }
 })
 
 router.get('/initialJournal', async (req, res) => {
     try {
-        const jdatas = { mvm1: 'wh2', mvm2: 'wh1', kmat: 'mat1', mnozstvi: 10, hmotnost: 10 };
-        const journalRec = new JournalRec(jdatas);
-        const journal = await journalRec.save();
-        res.json(journal);
+        await JournalRec.deleteMany();
+        const jDatas = [
+            { mvm1: 'wh2', mvm2: 'wh1', kmat: 'mat1', mnozstvi: 10, hmotnost: 10 },
+            { mvm1: 'wh1', mvm2: 'wh1', kmat: 'mat22', mnozstvi: 1001, hmotnost: 100 },
+            { mvm1: 'wh3', mvm2: 'wh2', kmat: 'mat3', mnozstvi: 10, hmotnost: 11 },
+            { mvm1: 'wh2', mvm2: 'wh1', kmat: 'mat1', mnozstvi: 10, hmotnost: 101 }
+        ];
+        await asyncForEach(jDatas, async (item) => {
+            const journalRec = new JournalRec(item);
+            await journalRec.save();
+        });
+        const journalOk = await JournalRec.find({});
+        res.json(journalOk);
     } catch(e) {
         console.error('unable to create initialData', e);
         res.status(500).json({ error: e });
