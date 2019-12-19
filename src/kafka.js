658383ddeaf084f832d24721d71fc6f83f7c2a9a
Petr Sorad <petr_sorad@cz.ibm.com>
2019-12-19 14:42:33 +0100

    consumer commit support

--- src/kafka.js
+++ src/kafka.js
@@ -1,6 +1,6 @@
 const kafkaHost = process.env.KAFKA_HOST
 const kafkaPort = process.env.KAFKA_PORT
-const kafkaHostEnv = process.env.KAFKA_HOST_ENV
+//const kafkaHostEnv = process.env.KAFKA_HOST_ENV
 const kafkaTopic = process.env.KAFKA_TOPIC
 const kafka = require('kafka-node')
 const JournalRec = require('./models/journalrec')
@@ -9,7 +9,7 @@ let mDate = new Date();
 let mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
 
 try {
-    console.log(mDateStr + ': Kafka Consumer is booting up ... (ENVs: kafkaHost:' + kafkaHost + '; kafkaTopic:' + kafkaTopic + '; kafkaHostEnv:' + kafkaHostEnv + '; )');
+    console.log(mDateStr + ': Kafka Consumer is booting up ... (ENVs: kafkaHost:' + kafkaHost + '; kafkaTopic:' + kafkaTopic + '; kafkaHost:' + kafkaHost + '; kafkaPort:' + kafkaPort + '; )');
     
 
     /*
@@ -26,7 +26,8 @@ try {
         }
     ]
     const options = {
-        autoCommit: true, 
+        // autoCommit: true, 
+        autoCommit: false, 
         fetchMaxWaitMs: 1000, 
         fetchMaxBytes: 1024 * 1024, 
         encoding: 'utf8', 
@@ -34,20 +35,51 @@ try {
     }
     
     const consumer = new kafka.Consumer(client, topics, options)
+    //consumer.setOffset(kafkaTopic, 0, 0);
 
+    client.on('ready', function(){
+        console.log('Client ready!');
+    });
+
+    consumer.on('offsetOutOfRange', function (err){
+        console.log("Kafka offsetOutOfRange: " + err);
+    });
+    
     consumer.on('message', async (message) => {    
-
+        mDate = new Date();
+        mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+        console.log(mDateStr + ': consumer.on() invoked.');
         const journalrec = new JournalRec(JSON.parse(message.value))
-        try {
-            await journalrec.save()
+        journalrec.save().then(function(data) {
+            consumer.commit(function (err, dta) {
+                if (err) {
+                    mDate = new Date();
+                    mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+                    console.log(mDateStr + ': Error: ' + err);
+                } else {
+                    mDate = new Date();
+                    mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+                    console.log(mDateStr + ': Commit success: ', dta);
+                }
+            });
             mDate = new Date();
             mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
             console.log(mDateStr + ': Journal record saved successfully: ' + message.value)
-        }catch(e) {
+        }).catch(function(error) {
             mDate = new Date();
             mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
-            console.log(mDateStr + ': journalrec.save() error:' + e)
-        }
+            console.log(mDateStr + ': journalrec.save() error:' + e);
+        });
+        // try {
+        //     await journalrec.save()
+        //     mDate = new Date();
+        //     mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+        //     console.log(mDateStr + ': Journal record saved successfully: ' + message.value)
+        // }catch(e) {
+        //     mDate = new Date();
+        //     mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+        //     console.log(mDateStr + ': journalrec.save() error:' + e)
+        // }
     })
 
     consumer.on('error', (err) => {
